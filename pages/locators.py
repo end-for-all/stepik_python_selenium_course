ef346ebf18e5e044564e85bd19aa8f53f40e1160
end-for-all <kazimird99@gmail.com>
2024-04-22 00:03:29 +0400

    added login_page.py, changes in test_guest_can_go_to_login_page

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_PAGE_LINK = "https://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    LOGIN_FORM = (By.ID, "login_form")
+    SIGN_UP_FORM = (By.ID, "register_form")
